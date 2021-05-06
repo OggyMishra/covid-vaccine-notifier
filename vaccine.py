7265ba4580d8d99ca5cd174863ca46465841ed3f
Sourabh Mishra <ms.sourabh@outlook.com>
2021-05-06 12:35:18 +0530

    Update vaccine.py

--- vaccine.py
+++ vaccine.py
@@ -13,32 +13,35 @@ def check_for_medicine(name, pin_code, date_range):
     import requests
     import json
     dates = [(datetime.now()+timedelta(days=i)).strftime('%d-%m-%Y') for i in range(0, date_range)]
-    slack_url = 'slack_hook' #Integrate the slack hook here.
+    slack_url = 'TODO: PASTE SLACK WEBHOOK HERE'
 
-    co_win_url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin'
+    co_win_url = 'https://api.cowin.gov.in/api/v2/appointment/sessions/public/findByPin'
     
     output = []
     for date in dates:
         url = f'{co_win_url}?pincode={pin_code}&date={date}'
-        output.append({'title': f'Vaccine status for {name} on {date} at pinCode: {pin_code}'})
-        r = requests.get(url)
-        res = r.json()
-        sessions = res['sessions']
-        valid_slots = list(filter(vaccine_condition, sessions))
-        if len(valid_slots) > 0:
-            for slot in valid_slots:
-                name = slot['name']
-                address = slot['address']
-                vaccine = slot['vaccine']
-                slots = slot['slots']
-                fee = slot['fee']
-                dt = slot['date']
-                output.append({ 'title': f'name: {name}, address: {address}, vaccine: {vaccine}, slots: {slots}, fee: {fee}, date: {dt}', 'short': False})
+        try:
+            r = requests.get(url)
+            res = r.json()
+            sessions = res['sessions']
+            valid_slots = list(filter(vaccine_condition, sessions))
+            if len(valid_slots) > 0:
+                output.append({'title': f'Vaccine status for {name} on {date} at pinCode: {pin_code}'})
+                for slot in valid_slots:
+                    name = slot['name']
+                    address = slot['address']
+                    vaccine = slot['vaccine']
+                    slots = slot['slots']
+                    fee = slot['fee']
+                    dt = slot['date']
+                    avl = slot['available_capacity']
+                    output.append({ 'title': f'name: {name}, address: {address}, vaccine: {vaccine}, slots: {slots}, fee: {fee}, date: {dt}, aval_capacity: {avl}', 'short': False})
 
-        else:
-            output.append({'title': 'No vaccine for you', 'short': False})
+        except Exception as e:
+            print(e)
 
-    payload = {
+    if len(output) > 0:
+        payload = {
             "attachments": [
                 {
                     "color": "#D00000",
@@ -46,8 +49,8 @@ def check_for_medicine(name, pin_code, date_range):
                 }
             ]
         }
-    payload = json.dumps(payload)
-    res = requests.post(slack_url, data=payload)
+        payload = json.dumps(payload)
+        requests.post(slack_url, data=payload)
 
 MIN_AGE = 44
 
@@ -56,8 +59,8 @@ if __name__ == '__main__':
     while True:
         name = 'Sourabh Mishra'
         pin_code = '110053'
-        date_range_check = 10
+        date_range_check = 3
         check_for_medicine(name, pin_code, date_range_check)
-        # run in every 2 hrs
-        time.sleep(7200)
+        # run in every 30 s
+        time.sleep(30)
    
